87b0b3c8b44aa6b133c09b46ce4fda78ccbc2621
thuy2 <thuy2@DESKTOP-ISH3HH8>
2021-03-12 14:07:51 +0700

    Add template for system

---
+++ selenium/webdriver/Topic_01_Template.java
@@ -0,0 +1,44 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Template {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+
+	@Test
+	public void TC_01() {
+		driver.get("");
+	}
+
+	@Test
+	public void TC_02() {
+
+	}
+
+	@Test
+	public void TC_03() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
