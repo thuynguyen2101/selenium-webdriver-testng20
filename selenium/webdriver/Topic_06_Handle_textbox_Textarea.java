6ba4e2417dcee13f81343d706d20ba352dd55997
thuy <thuy@podfoods.co>
2021-06-17 11:11:51 +0700

    Handle textbox Textarea

--- selenium/webdriver/Topic_06_Handle_textbox_Textarea.java
+++ selenium/webdriver/Topic_06_Handle_textbox_Textarea.java
@@ -6,14 +6,14 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Topic_06_Handle_textbox_Textarea {
 	WebDriver driver;
-	String emailAddress;
-	String loginPageUlr;
+	String emailAddress,loginPageUlr,userID,password;
 
 	@BeforeClass
 	public void beforeClass() {
@@ -21,44 +21,42 @@ public class Topic_06_Handle_textbox_Textarea {
 		driver.manage().window().maximize();
 		driver.get("http://demo.guru99.com/v4/");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
 		emailAddress=getrandomEmail();
 		
 	}
 
 	@Test
-	public void Register() {
+	public void TC_01_Register() {
 		loginPageUlr= driver.getCurrentUrl();
 		driver.findElement(By.xpath("//a[text()='here']")).click();
 		driver.findElement(By.xpath("//input[@name='emailid']")).sendKeys(emailAddress);
 		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
-		
-		String userID= driver.findElement(By.xpath("//td[text()='User ID :']//following-sibling::td")).getText();
-		String password= driver.findElement(By.xpath("//td[text()='Password :']//following-sibling::td")).getText();
-		
+		userID= driver.findElement(By.xpath("//td[text()='User ID :']//following-sibling::td")).getText();
+		password= driver.findElement(By.xpath("//td[text()='Password :']//following-sibling::td")).getText();
 		
 	}
 
 	@Test
-	public void Login() {
+	public void TC_02_Login() {
 		driver.get(loginPageUlr);
-		driver.findElement(By.name("uid")).sendKeys();
-		driver.findElement(By.name("password")).sendKeys();
+		driver.findElement(By.name("uid")).sendKeys(userID);
+		driver.findElement(By.name("password")).sendKeys(password);
 		driver.findElement(By.name("btnLogin")).click();
-		
-
+		Assert.assertEquals(driver.findElement(By.xpath("//marquee[@class='heading3']")).getText(), "Welcome To Manager's Page of Guru99 Bank");
 	}
 
 	@Test
-	public void Create_new_Customer() {
+	public void TC_03_Create_new_Customer() {
+		driver.findElement(By.xpath("//a[text()='New Customer']")).click();
+		driver.findElement(By.name("name")).sendKeys("Nguyen Thuy");
 
 	}
 	@Test
-	public void Edit_cusomter() {
+	public void TC_04_Edit_cusomter() {
 
 	}
 	@Test
-	public void Delete_customer() {
+	public void TC_04_Delete_customer() {
 
 	}
 	@AfterClass
